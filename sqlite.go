7fa5693ef868e53dc296370d450cb1ed699ff7ed
yihhao wang <2633565580@qq.com>
2024-04-01 15:53:46 +0800

    feat(sqlite): finished sqlite driver adapter

---
+++ sqlite.go
@@ -0,0 +1,37 @@
+package ent_sqlite
+
+import (
+	"context"
+	"database/sql"
+	"database/sql/driver"
+	"fmt"
+	"modernc.org/sqlite"
+)
+
+func init() {
+	sql.Register("sqlite3", Driver{&sqlite.Driver{}})
+}
+
+// Driver is sqlite driver wrapper
+type Driver struct {
+	*sqlite.Driver
+}
+
+func (d Driver) Open(dsn string) (driver.Conn, error) {
+	conn, err := d.Driver.Open(dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// ent sqlite must be enabled with fk
+	if execer, ok := conn.(driver.ExecerContext); ok {
+		_, err := execer.ExecContext(context.Background(), "PRAGMA foreign_keys = on;", nil)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		return nil, fmt.Errorf("unsupported driver connection")
+	}
+
+	return conn, nil
+}
